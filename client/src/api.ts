0976b8eabe31bb0645d9488d6173a215e6fb9727
KANAGALAKSHMI A <kanagaarun1808@gmail.com>
2026-04-24 23:46:39 +0530

    new updates 2

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://quizz-out.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
